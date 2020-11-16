15746e7d0b2e648325721551d991dcfead67f3b7
Ranisolanki <59246445+rani-solanki@users.noreply.github.com>
2020-11-16 13:45:04 +0530

    Add files via upload

---
+++ request1.py
@@ -0,0 +1,40 @@
+# import requests
+# import json
+
+# link="https://saral.navgurukul.org/course/?id=60&slug=samvaad-tech-201__howtouse"
+# response=requests.get(link)
+# print(response)
+# import requests
+# import json
+# link="http://saral.navgurukul.org/api/courses"
+# response=requests.get(link)
+# print(response)
+
+# def course(link):
+#     respones = requests.get(link)
+#     ret = respones.json()
+#     return ret
+#     print (ret)
+# course(link)
+
+# course_url = link+"/"+"courses" 
+# full_courses = course(course_url)
+# print (full_courses) 
+
+# course_list = []
+# def courses_func():
+#     index = 0
+#     while index < len(full_courses["availableCourses"]):
+#         courses_ex = full_courses["availableCourses"][index]
+#         courses_name = courses_ex["name"]
+#         courses_id = courses_ex["id"]
+#         course_list.append(courses_id)
+#         print (str(index)+"",courses_name,coimport_requests)
+# import json
+# saral_url = "http://saral.navgurukul.org/api"
+# print(saral_url)
+
+# def course(link):
+#     respones = (requests.get(link),urses_id)
+#         # index = index +1
+# courses_func() 
